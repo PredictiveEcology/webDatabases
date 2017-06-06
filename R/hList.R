2f2503dcb480e0657a3ce73ed80f2d983e08b5af
Alex Chubaty <alex.chubaty@gmail.com>
2017-06-06 13:53:00 -0600

    more cleanup

--- R/hList.R
+++ R/hList.R
@@ -22,19 +22,20 @@
 #' @examples
 #' outdir<- tempdir()
 #' file.list<- list.files(outdir)
-#' hfile <-hList(file.list, destfile = outdir, quick = TRUE)
-hList <-function(fList, destfile, quick = TRUE, csalgorithm = "xxhash64"){
+#' hfile <- hList(file.list, destfile = outdir, quick = TRUE)
+#'
+hList <- function(fList, destfile, quick = TRUE, csalgorithm = "xxhash64") {
   path2file <- lapply(file.path(destfile, fList), function(x) {x[!file.info(x)$isdir]})
   fList <- unlist(lapply(path2file, function(x) {x[file.exists(x), drop = FALSE]}))
 
-  if (length(fList)==0){
-    hdata<- data.frame(Filename=character(),  checksumFile=character(),
-                       checksumSize=character(), algorithm=character(), stringsAsFactors=FALSE)
-  }else{
+  if (length(fList) == 0) {
+    hdata <- data.frame(Filename = character(),  checksumFile = character(),
+                        checksumSize = character(), algorithm = character(), stringsAsFactors = FALSE)
+  } else {
     # Extract filenames and hash value from files downloaded locally.
-    if (!quick){
+    if (!quick) {
       # checksum on file
-      htag <- lapply(basename(fList), function(i) digest(file =file.path(destfile, i), algo = csalgorithm))
+      htag <- lapply(basename(fList), function(i) digest(file = file.path(destfile, i), algo = csalgorithm))
 
       # checksum on folder using folder name and size
       flst <- list.files(destfile)
@@ -42,38 +43,39 @@ hList <-function(fList, destfile, quick = TRUE, csalgorithm = "xxhash64"){
       allFiles <- lapply(flst, function(x) {list(basename(x), file.info(x)[,"size"])})
 
       hfolder <- digest(allFiles, algo = csalgorithm)
-      checkfolder<-data.frame(Filename=basename(destfile), checksumFile= NA,
-                              checksumSize=unlist(hfolder), algorithm = csalgorithm, stringsAsFactors=FALSE)
+      checkfolder < -data.frame(Filename = basename(destfile), checksumFile = NA,
+                                checksumSize = unlist(hfolder), algorithm = csalgorithm,
+                                stringsAsFactors = FALSE)
 
       # checksum on files using filename and size
-      fls<- subset(allFiles, unlist(allFiles)[ c(TRUE,FALSE) ] %in% basename(fList))
-      hfile <- lapply(fls, function(i) digest(i, algo= csalgorithm))
+      fls <- subset(allFiles, unlist(allFiles)[ c(TRUE,FALSE) ] %in% basename(fList))
+      hfile <- lapply(fls, function(i) digest(i, algo = csalgorithm))
 
       # Fill hdata data.frame
-      checkfiles<-data.frame(Filename=basename(fList), checksumFile= unlist(htag),
-                             checksumSize=unlist(hfile), algorithm = csalgorithm, stringsAsFactors=FALSE)
+      checkfiles < -data.frame(Filename = basename(fList), checksumFile = unlist(htag),
+                               checksumSize = unlist(hfile), algorithm = csalgorithm,
+                               stringsAsFactors = FALSE)
       hdata <- rbind(checkfiles, checkfolder)
-    } else{
+    } else {
       # checksum on folder using folder name and size
       flst <- list.files(destfile)
-      flst <- file.path(destfile,flst)
+      flst <- file.path(destfile, flst)
       allFiles <- lapply(flst, function(x) {list(basename(x), file.info(x)[,"size"])})
 
-      hfolder <- digest(allFiles, algo= csalgorithm)
-      checkfolder<-data.frame(Filename=basename(destfile), checksumFile= NA,
-                              checksumSize=unlist(hfolder), algorithm = csalgorithm, stringsAsFactors=FALSE)
+      hfolder <- digest(allFiles, algo = csalgorithm)
+      checkfolder <- data.frame(Filename = basename(destfile), checksumFile = NA,
+                                checksumSize = unlist(hfolder), algorithm = csalgorithm,
+                                stringsAsFactors = FALSE)
       # checksum on files using ilename and size
-      fls<- subset(allFiles, unlist(allFiles)[ c(TRUE,FALSE) ] %in% basename(fList))
-      hfile <- lapply(fls, function(i) {digest(i, algo= csalgorithm)})
+      fls <- subset(allFiles, unlist(allFiles)[ c(TRUE,FALSE) ] %in% basename(fList))
+      hfile <- lapply(fls, function(i) {digest(i, algo = csalgorithm)})
 
       # Fill hdata data.frame
-      checkfiles<-data.frame(Filename=basename(fList), checksumFile= NA,
-                             checksumSize=unlist(hfile), algorithm = csalgorithm, stringsAsFactors=FALSE)
+      checkfiles <- data.frame(Filename = basename(fList), checksumFile = NA,
+                               checksumSize = unlist(hfile), algorithm = csalgorithm,
+                               stringsAsFactors = FALSE)
       hdata <- rbind(checkfiles, checkfolder)
     }
   }
   return(hdata)
 }
-
-
-
